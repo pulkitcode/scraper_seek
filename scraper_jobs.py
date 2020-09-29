2b1019c9c86b49597bf68827a9586ff889a5e97b
Sanjog Gururaj <54097885+sanjogururaj47@users.noreply.github.com>
2020-09-29 20:33:37 +0530

    scraper bug fixes

--- scraper_jobs.py
+++ scraper_jobs.py
@@ -7,8 +7,8 @@ r = requests.get(URL)
 
 soup = BeautifulSoup(r.content, 'html.parser') 
 quotes=[]  # a list to store quotes 
-table = soup.find('article', class_ = "aria-label")
-print(table)
+table = soup.find('h1') 
+print(table.text)
 
 for row in table.findAll('div', 
                          attrs = {'data-search-sol-meta':'{"searchRequestToken":"241a0b2b-355b-461c-bd26-5a747d0ae416","jobId":"50566707";,"section";:"TOP","sectionRank":2,"jobAdType":"SPONSORED","tags":{"mordor:flights":"mordor_49","seek:innerWidth":"717","seek:innerHeight":"621","seek:customVersion":"9300","seek:page":"SERP"},"eventTime":1601380736420,"type":"ACTIVITY","activityType":"IMPRESSION"}'}): 
@@ -17,8 +17,9 @@ for row in table.findAll('div',
     quote['url'] = row.a['href'] 
     quotes.append(quote) 
 filename = 'inspirational_quotes.csv'
-with open(filename, 'w', newline='') as f: 
+with open(filename, 'w') as f: 
     w = csv.DictWriter(f,['url']) 
     w.writeheader() 
-    for quote in quotes: 
+    for quote in quotes:
+        print(quote)
         w.writerow(quote)
